6402e2ab922a86fe3dbd6ea3a77b2159abc975f3
Robilol <pimous78920@gmail.com>
2017-10-06 10:07:01 +0200

    first commit

---
+++ code/index.php
@@ -0,0 +1,10 @@
+<?php
+try
+{
+    $dbh = new PDO('mysql:host=esgi-db;dbname=esgi;port=3306', 'root', 'root');
+}
+catch (Exception $e)
+{
+	echo "bla";
+    die('Erreur : ' . $e->getMessage());
+}
