98557f882a74b4eea6fc95ad1aa9f479c2a4d581
Robilol <pimous78920@gmail.com>
2017-10-06 10:31:42 +0200

    rename container

--- code/index.php
+++ code/index.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-    $pdo = new PDO('mysql:host=esgi-db;dbname=esgi;port=3306', 'root', 'root');
+    $pdo = new PDO('mysql:host=esgi-db-1;dbname=esgi;port=3306', 'root', 'root');
 }
 catch (Exception $e)
 {
